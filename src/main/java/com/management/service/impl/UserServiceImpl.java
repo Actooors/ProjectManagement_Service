ee2bebb6b487fb7f7f62211da582bfbb2d02723f
0GGmr0 <18817619557@163.com>
2018-12-18 01:03:19 +0800

    fix(UserServiceImpl):修复了获取全部可申报项目的bug

--- src/main/java/com/management/service/impl/UserServiceImpl.java
+++ src/main/java/com/management/service/impl/UserServiceImpl.java
@@ -272,15 +272,19 @@ public class UserServiceImpl implements UserService {
 
         ProjectCategoryExample example = new ProjectCategoryExample();
         example.createCriteria()
-                .andApplicationEndTimeGreaterThan(new Date())
-                .andProjectCategoryIdIsNotNull();
+                .andApplicationEndTimeGreaterThan(new Date());
         List<ProjectCategory> projectCategoryList = projectCategoryMapper
                 .selectByExample(example);
+        if(projectCategoryList.isEmpty()) {
+            return ResultTool.success(new AviProjectCategoryInfoList());
+        }
+        //根据项目类别的int值排序
         Collections.sort(projectCategoryList);
         //获取到最大的项目类别
         int maxType = projectCategoryList.get(0).getProjectType();
         List<AviProjectCategoryInfoList> resList = new LinkedList<>();
-
+        //根据排序好的结果，先遍历的创建包含这些type的内容，
+        // 然后去遍历projectCategoryList内容找到符合当前条件的加进去
         for(int i = maxType, count = 0, len = projectCategoryList.size(); i >= 1; i--) {
             AviProjectCategoryInfoList singleList = new AviProjectCategoryInfoList();
             singleList.setType(ConstCorrespond.projectType[i]);

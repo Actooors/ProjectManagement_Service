582fbeaacc4972f29b4fa619ea1e4677fefca4bf
MistletoeXW <15178931042@qq.com>
2019-05-06 10:30:28 +0800

    fix:修改上传下载问题

--- src/main/java/com/management/model/ov/resultsetting/ConstCorrespond.java
+++ src/main/java/com/management/model/ov/resultsetting/ConstCorrespond.java
@@ -37,7 +37,7 @@ public class ConstCorrespond {
             "任务书审核阶段",
     };
 
-    public final static String downloadAddress = "http://129.204.71.113:8888/api/file/download?fileAddress=";
+    public final static String downloadAddress = "http://118.25.130.89:8081/api/file/download?fileAddress=";
 
     public final static String[] PROJECT_PROGRESS = {
             "",

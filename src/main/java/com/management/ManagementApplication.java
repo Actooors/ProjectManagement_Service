a855cf552cff54f17db0a5c2773cbb28b73f1aab
0GGmr0 <18817619557@163.com>
2019-08-15 08:29:06 +0800

    fix:修改时区问题

--- src/main/java/com/management/ManagementApplication.java
+++ src/main/java/com/management/ManagementApplication.java
@@ -5,10 +5,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @MapperScan("com.management.dao")
 public class ManagementApplication {
-
+    @PostConstruct
+    void setDefaultTimezone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+//    TimeZone.setDefault(TimeZone.getTimeZone("GMT+8"));
+    }
     public static void main(String[] args) {
         SpringApplication.run(ManagementApplication.class, args);
     }
